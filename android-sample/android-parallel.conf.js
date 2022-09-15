71777230c66e542ec6952e177795a657884ea8df
deepanshusri <103935065+deepanshusri@users.noreply.github.com>
2022-09-15 21:12:10 +0530

    Update android-parallel.conf.js

--- android-sample/android-parallel.conf.js
+++ android-sample/android-parallel.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
-  user: process.env.LT_USERNAME || "deepanshulambdatest",
-  key: process.env.LT_ACCESS_KEY || "jlBs32Zt7Q6Omx3ZThhWz1GTAzNQe96m7MP2Z6645UuZ2oVqTo",
+  user: process.env.LT_USERNAME || "YOUR_LT_ACCESS_KEY",
+  key: process.env.LT_ACCESS_KEY || "YOUR_ACCESS_KEY",
 
   updateJob: false,
   specs: ["specs/android-test.js"],
